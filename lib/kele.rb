f6b9e8996d6f560a55668ebe417853d4381765ca
jmuldvp <jmul.dvp@gmail.com>
2017-02-09 17:59:46 -0700

    CP 2 and 3

---
+++ lib/kele.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+class Kele
+  include HTTParty
+
+  def initialize(email, password)
+    response = self.class.post(api_end_point("sessions"), body: {"email": email, "password": password})
+    puts response.code
+    raise "Error" if response.code == 404
+    @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(api_end_point("users/me"), headers: {"authorization" => @auth_token})
+    @user_data = JSON.parse(response.body)
+  end
+
+  private
+  def api_end_point(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
+  end
+end
